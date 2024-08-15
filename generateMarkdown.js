75d23a5f147c7c8f328f3a88a72c415d23956ff0
oglesby80 <OglesbyRodriguez@gmail.com>
2024-08-15 13:16:07 -0400

    updated Markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) return '';
+  const badgeUrls = {
+    'MIT': 'https://img.shield.io/badge/Licence-MIT-yellow.svg',
+    'GPL': 'https://img.shield.io/badge/Licence-MIT-blue.svg',
+    'Apache 2.0':'https://img.shield.io/badge/Licence-Apache%202.0-blue.svg',
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
