c71a60c9f38196c4ce50cecd0d68bcd397c55396
oglesby80 <OglesbyRodriguez@gmail.com>
2024-08-15 20:15:32 -0400

    full update

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# challenge-7
-Homework
+# Challenge-7
+
+![License](https://img.shields.io/badge/License-GPL-blue.svg)
+
+## Description
+to create a ReadMe file
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+`npm install`
+
+## Usage
+`Please fill out all Sections`
+
+## License
+This project is licensed under the [GPL](https://opensource.org/licenses/GPL-3.0) license.
+
+## Contributing
+Please commit often
+
+## Tests
+To run tests, run the following command:
+`npm test`
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at Oglesbyrodriguez@gmail.com. You can find more of my work at [Oglesby80](https://github.com/Oglesby80).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,71 +1,66 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function to render license badge
 function renderLicenseBadge(license) {
   if (!license) return '';
   const badgeUrls = {
-    'MIT': 'https://img.shield.io/badge/Licence-MIT-yellow.svg',
-    'GPL': 'https://img.shield.io/badge/Licence-MIT-blue.svg',
-    'Apache 2.0':'https://img.shield.io/badge/Licence-Apache%202.0-blue.svg',
-    
+    'MIT': 'https://img.shields.io/badge/License-MIT-blue.svg',
+    'GPL': 'https://img.shields.io/badge/License-GPL-blue.svg',
+    'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
   };
-  return '![License](${badgeUrls[license]})';
+  return `![License](${badgeUrls[license]})`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function to render license link
 function renderLicenseLink(license) {
   if (!license) return '';
-  const licenseUrls = {
+  const licenseLinks = {
     'MIT': 'https://opensource.org/licenses/MIT',
-    'GPL': 'https://opensource.org/licenses/GPL',
-    'Apache 2.0':'https://opensource.org/licenses/Apache-2.0',
+    'GPL': 'https://opensource.org/licenses/GPL-3.0',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
   };
-  return LicenseLinks [license];
+  return licenseLinks[license]; // Fixed variable name here
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function to render license section
 function renderLicenseSection(license) {
   if (!license) return '';
   return `## License
-  This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
-  ## Description
-  ${data.description}
 
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
+${renderLicenseBadge(data.license)}
 
-  ## Installation
-  ${data.installation}
+## Description
+${data.description}
 
-  ## Usage
-  ${data.usage}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-  ${renderLicenseSection(data.license)}
+## Installation
+\`${data.installation}\`
 
-  ## Contributing
-  ${data.contributing}
+## Usage
+\`${data.usage}\`
 
-  ## Tests
-  ${data.tests}
+${renderLicenseSection(data.license)}
 
-  ## Questions
-  If you have any questions, Feel free to contact me:
-  - GitHub: [${data.github}](https://github.com/${data.github})
-  - Email: ${data.email}
+## Contributing
+${data.contributing}
 
+## Tests
+To run tests, run the following command:
+\`${data.tests}\`
 
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}).
 `;
 }
 
